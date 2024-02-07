9e3e146ec3ed09ed47b196d87cfb8eeed224370b
Minatonton <minatonton1207@gmail.com>
2024-02-07 11:13:03 +0900

    kousin

--- graph/TopologicalSort.cpp
+++ graph/TopologicalSort.cpp
@@ -2,3 +2,35 @@
 using namespace std;
 #define ll long long
 #define rep(i,n) for(ll i=0;i<(ll)(n);i++)
+
+
+//求めたいトポロジカルソートしたものが辞書順の場合、queueの代わりに
+// priority_queueを使う
+vector<ll>topological_sort(const vector<vector<ll>>&G){
+    const ll n=G.size();
+    vector<ll>deg(n);
+    for(ll v=0;v<n;v++){
+        for(auto nv:G[v]){
+            deg[nv]++;
+        }
+    }
+    vector<ll>res;
+    queue<ll>que;
+    for(ll v=0;v<n;v++){
+        if(deg[v]==0){
+            que.push(v);
+            res.push_back(v);
+        }
+    }
+    while(!que.empty()){
+        ll v=que.front();que.pop();
+        for(auto nv:G[v]){
+            deg[nv]--;
+            if(deg[nv]==0){
+                que.push(nv);
+                res.push_back(nv);
+            }
+        }
+    }
+    return res;
+}
